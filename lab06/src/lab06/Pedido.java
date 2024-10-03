8049477ec946e7dfe0fcba839c1c4c6a241fe3f2
COOKIEMONSTER133 <rodman.velazco@ucsm.edu.pe>
2024-10-03 09:49:18 -0500

    .....

---
+++ lab06/src/lab06/Pedido.java
@@ -0,0 +1,13 @@
+package lab06;
+
+public class Pedido {
+    private String nombrePlato;
+
+    public Pedido(String nombrePlato) {
+        this.nombrePlato = nombrePlato;
+    }
+
+    public String getNombrePlato() {
+        return nombrePlato;
+    }
+}
